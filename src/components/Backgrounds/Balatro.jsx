9f1fc8bde017ebf77adf7dac83e1c609f21e106d
Ryonandha <ryonandhar@gmail.com>
2026-01-25 01:04:21 +0700

    Update Progress Pagi Ini

---
+++ src/components/Backgrounds/Balatro.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+<div style={{ width: "1080px", height: "1080px", position: "relative" }}>
+  <Balatro spinRotation={-2} spinSpeed={7} color1="#DE443B" color2="#006BB4" color3="#162325" contrast={3.5} lighting={0.4} spinAmount={0.25} pixelFilter={700} />
+</div>;
+
+export default Balatro;
